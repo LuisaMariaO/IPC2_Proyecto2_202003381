f5ebbe55fe4109d452cd31a9a6bcbebc1562392b
Luisa María Ortíz Romero <78897324+LuisaMariaO@users.noreply.github.com>
2021-09-12 21:11:42 -0600

    Add: Lector de archivos en nueva interfaz

--- main.py
+++ main.py
@@ -1,70 +1,33 @@
-from tkinter import *
-from tkinter.filedialog import askopenfilename
-import os
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QAction, QMessageBox, QDialog
+from Interfaz import MainWindow
+import xml.etree.ElementTree as ET
 
 
-def cargaMaquina():
-    
-    filename=askopenfilename(filetypes=[("Archivo xml","*.xml")])
+class Ventana(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui=MainWindow()
+        self.ui.setupUi(self)
+        self.ui.action_config_maquina.triggered.connect(self.config)
 
-def ventanaPrincipal():
-    #Termina la pantalla de carga
-    loading.destroy()
-    #Configuración general de la ventana principal
-    ventana=Tk()
-    ancho=ventana.winfo_screenwidth()
-    
-    alto=ventana.winfo_screenheight()
-    print(ancho)
-    print(alto)
-    ventana.geometry("%dx%d" % (ancho, alto))
-    ventana.title("Digital Intelligence S.A.")
-    ruta_abs=os.path.abspath('.')
-    ventana.iconbitmap('Imagenes\\icono.ico')
-    
-    #Boton de configuración de máquina
-    img_config = PhotoImage(file='Imagenes\\settings.png')
-    boton_config=Button(text="Cargar configuración",image=img_config, compound=TOP, height=60, width=115, command=cargaMaquina)
-    boton_config.place(x=(ancho-125),y=0)
-
-    #Carga de archivo
-    img_loadfile = PhotoImage(file='Imagenes\\load.png')
-    boton_cargar=Button(text="Cargar productos",image=img_loadfile, compound=TOP, height=60, width=90)
-    boton_cargar.place(x=0,y=0)
-    
-    #Exportar reporte
-    img_expfile = PhotoImage(file='Imagenes\\export.png')
-    boton_exp=Button(text="Exportar reportes",image=img_expfile, compound=TOP, height=60, width=90)
-    boton_exp.place(x=99,y=0)
-
-    #Ayuda
-    img_help = PhotoImage(file='Imagenes\\help.png')
-    boton_help=Button(text="Ayuda",image=img_help, compound=TOP, height=60, width=90)
-    boton_help.place(x=198,y=0)
-
-    ventana.mainloop()
-
-  
-    
+        #self.pushButton.clicked.connect(self.hola)
+    def config(self):
+        archivo=QFileDialog.getOpenFileName(self, 'Abrir archivo', 'C:\\','Archivos xml (*.xml)')
+        try:
+        
+            tree = ET.parse(archivo[0])
+            root = tree.getroot()   
+        except:
+            QMessageBox.about(self, "Advertencia", "No es posible abrir el archivo")
 
 
-loading=Tk()
-loading.title("Cargando")
-x=0
-y=0
-x=(loading.winfo_screenwidth())//2-250
-    
-y=(loading.winfo_screenheight())//2-250
-
-loading.geometry("500x500"+"+"+str(x)+"+"+str(y))
-loading.overrideredirect(1)
-
-img = PhotoImage(file = 'Imagenes\\Logo.png')
-label_img = Label(loading, image = img)
-label_img.pack()
 
 
-loading.resizable(0,0)
-loading.after(3000, ventanaPrincipal)
-mainloop()
 
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    GUI=Ventana()
+    GUI.show()
+    sys.exit(app.exec_())
