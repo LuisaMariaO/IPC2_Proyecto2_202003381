5b390b7344f8a22a41b62e86878b13f4ef165321
Luisa María Ortíz Romero <78897324+LuisaMariaO@users.noreply.github.com>
2021-09-26 17:35:57 -0600

    Delete ListaProductosSimulados.py

--- ListaProductosSimulados.py
+++
@@ -1,6 +0,0 @@
-from ProductoSimulado import ProductoSimulado
-class ListaProductosSimulados:
-    def __init__(self):
-        self.primero = None
-        self.ultimo = None
-        self.size = 0
