84ac5497dfb10b74be5c7684912075fd945acb11
Luisa María Ortíz Romero <78897324+LuisaMariaO@users.noreply.github.com>
2021-09-26 17:37:29 -0600

    Delete Simulacion.py

--- Simulacion.py
+++
@@ -1,7 +0,0 @@
-from ListaProductosSimulados import ListaProductosSimulados
-class Simulacion():
-    def __init__(self,nombre):
-        self.nombre=nombre
-        self.productos_simulados=ListaProductosSimulados()
-        self.siguiente = None
-        self.anterior = None
