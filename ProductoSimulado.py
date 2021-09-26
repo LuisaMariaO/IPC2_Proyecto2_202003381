1234035462cf5184c6bf2e9450617b4566ea0b63
Luisa María Ortíz Romero <78897324+LuisaMariaO@users.noreply.github.com>
2021-09-26 17:36:33 -0600

    Delete ProductoSimulado.py

--- ProductoSimulado.py
+++
@@ -1,8 +0,0 @@
-from ElaboracionOptima import ElaboracionOptima
-class ProductoSimulado():
-    def __init__(self,nombre,tiempoTotal):
-        self.nombre=nombre
-        self.tiempoTotal=tiempoTotal
-        self.elaboracionOptima=ElaboracionOptima()
-        self.siguiente = None
-        self.anterior = None
